dcddbe36e48367ac2807c68ac0d76bbf1ce4ac3c
sagar-roy109 <sagar.roy.freelancer@gmail.com>
2023-04-21 11:32:39 -0400

    mobile menu added

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import {NavLink, Link} from 'react-router-dom';
 import { AiOutlineUser, AiOutlineHome, AiOutlineFileText, AiOutlineCodepen } from "react-icons/ai";
+import { FiBarChart2 } from "react-icons/fi";
+import { useRef, useState } from 'react';
+
 import "../css/Header.css";
 
 const HeaderComponent = () => {
+	const navMenuRef = useRef();
+	const mobileMenuShow = useState(false);
+	const handleMobileMenu = ()=>{
+		navMenuRef.current.classList.toggle('mobile_menu');
+
+	}
+
+
 	return (
 		<>
 			<nav>
@@ -12,13 +23,16 @@ const HeaderComponent = () => {
 						<div className="nav__logo">
 							<Link to = '/'>WEB PRO</Link>
 						</div>
-						<div className="nav__menu">
+						<div className="nav__menu" ref={navMenuRef}>
 							<ul>
 								<li><NavLink to={'/'}><span><AiOutlineHome/></span>Home</NavLink></li>
 								<li><NavLink to={'/about'}><span><AiOutlineUser/></span>About</NavLink></li>
 								<li><NavLink to={'/resume'}><span><AiOutlineFileText/></span>Resume</NavLink></li>
 								<li><NavLink to={'/portfolio'}><span><AiOutlineCodepen/></span>Portfolio</NavLink></li>
 							</ul>
+						</div>
+						<div className="mobile__menu_icon">
+							<FiBarChart2 onClick={handleMobileMenu}/>
 						</div>
 					</div>
 				</div>
